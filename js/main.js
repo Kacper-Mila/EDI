0ceb11f8fcd08cab90b0e664781355b3e662f2a6
Kacper-Mila <kacper.mila@gmail.com>
2024-01-02 21:50:25 +0100

    moved charts to second page and made them use the data fetched from main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const apiUrl = "https://my.api.mockaroo.com/some_schematic.json?key=d7234cf0";
-const mockUrl = "../assets/data/Some_schematic.json";
+const mockUrl = "../assets/data/some_schematic.json";
 // use this to manipulate the data from the API
 let data = [];
 
